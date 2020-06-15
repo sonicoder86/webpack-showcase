ba29f9a296a42bba86bf938873c6ed2d1855a77b
blacksonic <soos.gabor86@gmail.com>
2020-06-15 11:46:25 +0200

    feat(babel): move browserlist config to separate file

--- webpack.config.js
+++ webpack.config.js
@@ -11,19 +11,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: [
-                    'last 2 Chrome versions',
-                    'last 2 Firefox versions',
-                    'last 2 Edge versions',
-                    'last 2 Safari versions',
-                  ]
-                }
-              ]
-            ]
+            presets: ['@babel/preset-env']
           }
         }
       }
